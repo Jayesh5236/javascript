6aa206ff93964c2ccd9d18e7b84a11154a56e7e8
Jayesh5236 <jayesh5236@gmail.com>
2024-01-22 14:53:18 +0530

    Strings

---
+++ basics/strings/1.js
@@ -0,0 +1,100 @@
+// var a = "Hello class, ";
+
+// console.log(a);
+
+// var b = "Welcome to JS";
+
+// console.log(a + b);
+
+//Different ways to write a string in JS
+
+//1. Double Quotes - ""
+//2. Single Quotes - ''
+//3. Backticks - `` ( template literals )
+
+var a = 40 , b = 30 , c = 10;
+var sum = a + b + c;
+// console.log(sum);
+
+// console.log("The sum of a - "+a+" , b - "+b+", c - "+c+" is => sum="+sum);
+
+//String Pollination
+// console.log(`The sum of a - ${a}, b - ${b}, c - ${c} is => sum = ${sum}`);
+//"https://github.com/TPAteeq/THSCohort-B-2023"
+
+// `https://github.com/${userName}/${repoName}`
+
+// var greeting = "Hello World"
+
+// console.log(greeting.length);
+// console.log(greeting[4]);
+// console.log(greeting[11]);
+
+// console.log(greeting.charAt(10));
+
+// console.log(greeting.indexOf("o"));
+// console.log(greeting.indexOf("H"));
+// console.log(greeting.indexOf("w"));
+
+// greeting = greeting.toLowerCase()
+// greeting = greeting.toUpperCase()
+
+// console.log(greeting);
+
+
+//Slice
+
+// console.log(greeting.slice(0,2));
+// console.log(greeting.slice(6));
+// console.log(greeting);
+// console.log(greeting.slice(-1));
+
+
+
+
+// Capitalize the word => hackingschool ( output - Hackingschool)
+// var word = "hackingschool"
+// var firstLetter = word[0]
+
+// firstLetter = firstLetter.toUpperCase()
+// var remainingWord = word.slice(1)
+// console.log(remainingWord);
+// console.log(firstLetter + remainingWord);
+
+// console.log(word[0].toUpperCase()+word.slice(1))
+
+// var text = "             HI    "
+// console.log(text.trim());
+
+// var word = "abc"
+// console.log(word.repeat(10));
+
+// var str = "Hello class, it is a cold day today, so cold"
+// console.log(str.replace("cold","warm"));
+// console.log(str.replaceAll("cold","warm"));
+
+// console.log("hello"+"world")
+
+
+//concat method
+// var hello = "hello";
+// console.log(hello.concat(" world", " jojo", " jonathan"))
+// console.log(hello);
+
+
+// var str = "aeroplane"
+// console.log(str.charCodeAt(0));
+// console.log(str.charCodeAt(8));
+
+// var str = "coding&is&easy"
+// console.log(str.split(""));
+// console.log(str.split(" "));
+// console.log(str.split("&"));
+
+// var arr = ["h","e","l","l","o"]
+// console.log(arr.join(""));
+
+// var string = "hello world";
+// var arr = string.split("")
+// arr.reverse()
+// console.log(arr.join("")); 
