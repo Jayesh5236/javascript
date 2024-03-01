db77857ce7a9d09787a23eb0c417c1fb2a70556a
Jayesh5236 <jayesh5236@gmail.com>
2024-03-01 23:12:15 +0530

    map method in react

--- reactquestion/src/App.jsx
+++ reactquestion/src/App.jsx
@@ -3,12 +3,42 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-const [count,setCount] =useState(0)
-  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-
+      <h1>Users</h1>
+      <div className="user">
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
+<hr />
+      <div className="user-title">
+        {users.map((user) => (
+          <div key={user.id}>{user.email}</div>
+        ))}
+      </div>
+<hr />
+      <div className="user-phone">
+        {users.map((user) => (
+          <div key={user.id}>{user.phone}</div>
+        ))}
+      </div>
     </div>
   );
 }
